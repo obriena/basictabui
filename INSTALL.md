7e96cc0d199a708245a29e8186a2740eeb14fcd5
obriena <aaron@flyingspheres.com>
2022-03-24 07:35:08 -0500

    base functionality is working

---
+++ INSTALL.md
@@ -0,0 +1,61 @@
+aaronobrien@air-de-aaron React % npx create-react-app serverstatus
+
+Creating a new React app in /Users/aaronobrien/Desarrollo/React/serverstatus.
+
+Installing packages. This might take a couple of minutes.
+Installing react, react-dom, and react-scripts with cra-template...
+
+
+added 1372 packages in 34s
+
+169 packages are looking for funding
+  run `npm fund` for details
+
+Initialized a git repository.
+
+Installing template dependencies using npm...
+npm WARN deprecated source-map-resolve@0.6.0: See https://github.com/lydell/source-map-resolve#deprecated
+
+added 38 packages in 2s
+
+169 packages are looking for funding
+  run `npm fund` for details
+Removing template package using npm...
+
+
+removed 1 package, and audited 1410 packages in 902ms
+
+169 packages are looking for funding
+  run `npm fund` for details
+
+6 moderate severity vulnerabilities
+
+To address all issues (including breaking changes), run:
+  npm audit fix --force
+
+Run `npm audit` for details.
+
+Created git commit.
+
+Success! Created serverstatus at /Users/aaronobrien/Desarrollo/React/serverstatus
+Inside that directory, you can run several commands:
+
+  npm start
+    Starts the development server.
+
+  npm run build
+    Bundles the app into static files for production.
+
+  npm test
+    Starts the test runner.
+
+  npm run eject
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory. If you do this, you can’t go back!
+
+We suggest that you begin by typing:
+
+  cd serverstatus
+  npm start
+
+Happy hacking!
--- src/App.js
+++ src/App.js
@@ -1,25 +1,166 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Header(props){
+  return(
+      <div>
+        <h1>Service Monitoring</h1>
+      </div>
+  )
+}
+
+class DevTools extends React.Component{
+  render(){
+    return (
+    <div className='devTools'>
+      <p className='bottom'>Dev Tools Live Here </p>
+      <p><input id="enableInternetId" 
+                type="checkbox" 
+                onClick={()=> this.props.onClick()} 
+                defaultChecked={this.props.internetEnabled}/> Internet Enabled </p>
     </div>
-  );
+    )
+  }
+}
+
+class ServicesContent extends React.Component{
+  render(){
+    return(
+      <div id={'servicesIdTab'}>
+        List Services
+      </div>
+    )
+  }
+}
+
+class TrendsContent extends React.Component{
+  render(){
+    return(
+      <div id={'trendsIdTab'} className='hiddenTab'>
+        Trends, or History of Service Performance
+      </div>
+    )
+  }
+}
+
+class NewServiceContent extends React.Component{
+  render(){
+    return(
+      <div id={'newServiceIdTab'} className='hiddenTab'>
+        New Service
+      </div>
+    )
+  }
+}
+
+class ScheduleContent extends React.Component{
+  render(){
+    return(
+      <div id={'scheduleIdTab'} className='hiddenTab'>
+        Schedule
+      </div>
+    )
+  }
+}
+
+class AboutContent extends React.Component{
+  render(){
+    return(
+      <div id={'aboutIdTab'} className='hiddenTab'>
+        About
+      </div>
+    )
+  }
+}
+
+class Menu extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+    <div>
+    <ul id={'menuListId'}>
+      <li>
+        <a id={'servicesId'} 
+            className="active" 
+            href="#services" 
+            onClick={()=> this.props.onClick('servicesId')}>Services</a>
+        </li>
+      <li>
+        <a id={'trendsId'} 
+           href="#trends" 
+           onClick={()=> this.props.onClick('trendsId')} >Trends</a>
+        </li>
+      <li>
+        <a id={'newServiceId'} href="#newService" onClick={()=> this.props.onClick('newServiceId')}>New Service</a>
+        </li>
+      <li>
+        <a id={'scheduleId'} href="#schedule" onClick={()=> this.props.onClick('scheduleId')}>Schedule</a>
+        </li>
+      <li>
+        <a id={'aboutId'} href="#about" onClick={()=> this.props.onClick('aboutId')}>About</a>
+        </li>
+    </ul>
+    </div>
+  )}
+}
+
+class App extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      internetEnabled: false,
+    }
+  }
+  render() {
+    console.log("Internet toggled is now: " + this.state.internetEnabled);
+    return(
+    <div className="App">
+      <Header />
+      <Menu onClick={(i)=>this.handleMenuClick(i)}/>
+      <ServicesContent/>
+      <TrendsContent/>
+      <NewServiceContent/>
+      <ScheduleContent/>
+      <AboutContent/>
+      <DevTools internetEnabled={this.state.internetEnabled} 
+                onClick={(i)=> this.toggleInternet(i)} />
+    </div>
+    )
+  }
+
+  handleMenuClick(i){
+    let menuList = document.querySelector('#menuListId');
+    let menuChildren = menuList.children;
+    for (let a = 0; a < menuChildren.length; a++){
+       let liEle = menuChildren[a];
+       let anchor  = liEle.children[0];
+       if (anchor.id === i){
+        anchor.className='active';
+       } else {
+        anchor.className='';
+       }
+    }
+    document.querySelector('#servicesIdTab').className='hiddenTab';
+    document.querySelector('#trendsIdTab').className='hiddenTab';
+    document.querySelector('#newServiceIdTab').className='hiddenTab';
+    document.querySelector('#scheduleIdTab').className='hiddenTab';
+    document.querySelector('#aboutIdTab').className='hiddenTab';
+    document.querySelector('#'+i+'Tab').className='';
+  }
+
+  toggleInternet(){
+    let checkBox = document.querySelector('#enableInternetId');
+    
+    this.setState ({
+      internetEnabled: checkBox.checked
+    })
+  }
 }
 
 export default App;
